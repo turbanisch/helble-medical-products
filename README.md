dddbf3e988046e856b78f1e006759b11089be591
turbanisch <fetu@gmx.de>
2022-10-24 19:01:54 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# helble-medical-products
+# Helble (2012) List of Medical Goods
+
 List of medical goods extracted from Helble, Matthias. 2012. “More Trade for Better Health? - International Trade and Tariffs on Health Products.” SSRN Scholarly Paper ID 2165734. Rochester, NY: Social Science Research Network. https://doi.org/10.2139/ssrn.2165734, pages 28-35.
+
+Procedure:
+1. import PDF with Word
+2. copy and paste table into Excel
+3. import Excel table into R and run `clean.R`
+    - clean data
+    - convert HS3 (2007) to HS5 (2017)
+4. save as `medical_products.csv`
+
+Notes:
+- 28 original HS codes appear to be not from HS3 but from earlier versions and were not converted
+- it is not explained in the paper what "old" refers to.
---
+++ clean.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(concordance)
+
+setwd("/Users/felix/Dropbox/China Group Customs/_Aid/data_external/medical_products")
+
+# load data
+df <- readxl::read_excel("list_medical_products.xlsx")
+
+# rename columns
+df <- df %>% 
+  filter(!is.na(Nr)) %>% 
+  rename(
+    no = Nr,
+    hs6_2007 = `HS Code`,
+    group = Group,
+    description = Description,
+    old = Old
+  ) %>% 
+  mutate(old = if_else(old == "*", TRUE, FALSE, missing = FALSE))
+
+# make sure no entries are missing
+setdiff(1:207, df$no)
+setdiff(df$no, 1:207)
+
+df <- df %>% 
+  select(!no)
+
+# convert from HS 3 (2007) to HS 5 (2017) 
+df <- df %>% 
+  mutate(hs6_2017 = concord(hs6_2007,
+                       origin = "HS3", 
+                       destination = "HS5",
+                       dest.digit = 6)) %>% 
+  relocate(starts_with("hs"))
+
+# 23 HS codes cannot be matched, because not in HS3 (but earlier versions), 184 correctly converted
+
+# iteratively look up NAs with earlier HS versions
+# df <- df %>%
+#   mutate(hs6_2017 = if_else(
+#     is.na(hs6_2017),
+#     concord(
+#       hs6_2007,
+#       origin = "HS2",
+#       destination = "HS5",
+#       dest.digit = 6
+#     ),
+#     hs6_2017)) %>% 
+#   mutate(hs6_2017 = if_else(
+#     is.na(hs6_2017),
+#     concord(
+#       hs6_2007,
+#       origin = "HS1",
+#       destination = "HS5",
+#       dest.digit = 6
+#     ),
+#     hs6_2017)) %>% 
+#   mutate(hs6_2017 = if_else(
+#     is.na(hs6_2017),
+#     concord(
+#       hs6_2007,
+#       origin = "HS0",
+#       destination = "HS5",
+#       dest.digit = 6
+#     ),
+#     hs6_2017))
+
+# save
+write_csv(df, "medical_products.csv")
