944dc4f43f95e71ddbd4da169d4fbdf37c725268
turbanisch <fetu@gmx.de>
2022-10-24 19:47:29 +0200

    rename variables and run

--- clean.R
+++ clean.R
@@ -2,66 +2,27 @@ library(tidyverse)
 library(concordance)
 
 # load data
-df <- readxl::read_excel("data/derived/list_medical_products.xlsx")
+df <- readxl::read_excel("data/derived/list_medical_products.xlsx",
+                         col_types = c("numeric", "text", "text", "text", "text"))
 
 # rename columns
 df <- df %>% 
-  filter(!is.na(Nr)) %>% 
+  select(!Nr) %>% 
   rename(
-    no = Nr,
-    hs6_2007 = `HS Code`,
+    hs3 = `HS Code  6 digit`,
     group = Group,
     description = Description,
     old = Old
   ) %>% 
   mutate(old = if_else(old == "*", TRUE, FALSE, missing = FALSE))
 
-# make sure no entries are missing
-setdiff(1:207, df$no)
-setdiff(df$no, 1:207)
-
-df <- df %>% 
-  select(!no)
-
 # convert from HS 3 (2007) to HS 5 (2017) 
 df <- df %>% 
-  mutate(hs6_2017 = concord(hs6_2007,
+  mutate(hs5 = concord(hs3,
                        origin = "HS3", 
                        destination = "HS5",
                        dest.digit = 6)) %>% 
   relocate(starts_with("hs"))
 
-# 23 HS codes cannot be matched, because not in HS3 (but earlier versions), 184 correctly converted
-
-# iteratively look up NAs with earlier HS versions
-# df <- df %>%
-#   mutate(hs6_2017 = if_else(
-#     is.na(hs6_2017),
-#     concord(
-#       hs6_2007,
-#       origin = "HS2",
-#       destination = "HS5",
-#       dest.digit = 6
-#     ),
-#     hs6_2017)) %>% 
-#   mutate(hs6_2017 = if_else(
-#     is.na(hs6_2017),
-#     concord(
-#       hs6_2007,
-#       origin = "HS1",
-#       destination = "HS5",
-#       dest.digit = 6
-#     ),
-#     hs6_2017)) %>% 
-#   mutate(hs6_2017 = if_else(
-#     is.na(hs6_2017),
-#     concord(
-#       hs6_2007,
-#       origin = "HS0",
-#       destination = "HS5",
-#       dest.digit = 6
-#     ),
-#     hs6_2017))
-
 # save
-write_csv(df, "medical_products.csv")
+write_csv(df, "output/medical_products.csv")
